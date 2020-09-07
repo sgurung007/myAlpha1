6699a964a80cdcd080575016ec7a1324e0b400d6
sgurung007 <51142761+sgurung007@users.noreply.github.com>
2020-09-07 11:02:45 -0500

    change 2 from github master

--- src/main/java/maven12/Alpha1/App.java
+++ src/main/java/maven12/Alpha1/App.java
@@ -17,6 +17,7 @@ public class App
         System.out.println("Welcome to oklahoma");
         System.out.println("change 1 from github in master branch");
         System.out.println("change 1 from eclipse in tail branch");
+        System.out.println("change 2 from github in master branch");
         
 
 
