93f0d2856d94819053bc1c3e0efc508426d7f05d
suraj gurung <sgrg007@gmail.com>
2020-09-07 09:52:14 -0500

    initial commit
    Signed-off-by: suraj gurung <sgrg007@gmail.com>

---
+++ src/test/java/maven12/Alpha1/AppTest.java
@@ -0,0 +1,20 @@
+package maven12.Alpha1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
